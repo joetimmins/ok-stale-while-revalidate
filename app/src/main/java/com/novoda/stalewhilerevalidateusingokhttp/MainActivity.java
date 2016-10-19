21b3f94e0843c53d63c240f522046e59774da978
Joe Timmins <joe@novoda.com>
2016-10-19 17:38:22 +0100

    Make a request

--- app/src/main/java/com/novoda/stalewhilerevalidateusingokhttp/MainActivity.java
+++ app/src/main/java/com/novoda/stalewhilerevalidateusingokhttp/MainActivity.java
@@ -1,13 +1,53 @@
 package com.novoda.stalewhilerevalidateusingokhttp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String GOOGLE_URL = "http://www.google.com";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Request request = new Request.Builder()
+                .url(GOOGLE_URL)
+                .build();
+        OkHttpClient client = new OkHttpClient.Builder().build();
+        Call call = client.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                String localizedMessage = e.getLocalizedMessage();
+                show(localizedMessage);
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String string = response.body().string();
+                show(string);
+            }
+        });
+    }
+
+    private void show(final String text) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                TextView textView = (TextView) findViewById(R.id.text_view);
+                textView.setText(text);
+            }
+        });
     }
 }
